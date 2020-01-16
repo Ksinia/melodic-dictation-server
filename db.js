8608996bc49ff2eb896cafb69fb1ea26b6cd1fc2
Ksinia <xiatian@mail.ru>
2020-01-16 12:33:10 +0100

    Add data to db

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const Sequelize = require("sequelize");
+
 const databaseUrl =
   process.env.DATABASE_URL ||
   "postgres://postgres:secret@localhost:5432/postgres";
 const db = new Sequelize(databaseUrl, { logging: false }); // to get rid of unnecessary messages in console
 
-db.sync({ force: true }) // dont delete data on sync
+db.sync({ force: false }) // dont delete data on sync
   .then(() => console.log("DB connected"));
 
 module.exports = db;
