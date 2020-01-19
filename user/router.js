45ebc5e7e070ff9fb90535f0db101bc693b57632
Ksinia <xiatian@mail.ru>
2020-01-19 13:37:26 +0100

    change .then usage to async

--- user/router.js
+++ user/router.js
@@ -5,41 +5,43 @@ const { login } = require("../auth/router");
 
 const router = new Router();
 
-router.post("/signup", (req, res, next) => {
+router.post("/signup", async (req, res, next) => {
   if (!req.body.password) {
     res.status(400).send({
       message: "Password should not be empty"
     });
     return;
   }
-  const user = {
-    name: req.body.name,
-    password: bcrypt.hashSync(req.body.password, 10)
-  };
-  User.findOne({ where: { name: req.body.name } }).then(data => {
-    if (data) {
-      res.status(400).send({
-        message: "This name is already in use"
-      });
-    } else {
-      User.create(user)
-        .then(user => login(res, next, user.name, req.body.password))
-        .catch(error => {
-          if (error.name === "SequelizeValidationError") {
-            console.log("Error errors[0] type:", typeof error.errors[0]);
-            console.log(
-              "errors[0] class name:",
-              error.errors[0].constructor.name
-            );
-            res.status(400).send({
-              message: error.errors[0].message
-            });
-          } else {
-            next(error);
-          }
+  let userWithSameName = null;
+  try {
+    userWithSameName = await User.findOne({
+      where: { name: req.body.name }
+    });
+  } catch (error) {
+    next(error);
+  }
+  if (userWithSameName) {
+    res.status(400).send({ message: "This name is already in use" });
+  } else {
+    const userData = {
+      name: req.body.name,
+      password: bcrypt.hashSync(req.body.password, 10)
+    };
+    try {
+      const user = await User.create(userData);
+      login(res, next, user.name, req.body.password);
+    } catch (error) {
+      if (error.name === "SequelizeValidationError") {
+        console.log("Error errors[0] type:", typeof error.errors[0]);
+        console.log("errors[0] class name:", error.errors[0].constructor.name);
+        res.status(400).send({
+          message: error.errors[0].message
         });
+      } else {
+        next(error);
+      }
     }
-  });
+  }
 });
 
 module.exports = router;
