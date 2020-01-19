12843e2129d9ebae3f21721b9e4da39a99aedc1d
Ksinia <xiatian@mail.ru>
2020-01-19 22:14:20 +0100

    Create initial migration

---
+++ migrations/1-init.js
@@ -0,0 +1,241 @@
+'use strict';
+
+var Sequelize = require('sequelize');
+
+/**
+ * Actions summary:
+ *
+ * createTable "users", deps: []
+ * createTable "melodies", deps: [users]
+ * createTable "dictations", deps: [users, melodies]
+ *
+ **/
+
+var info = {
+    "revision": 1,
+    "name": "noname",
+    "created": "2020-01-19T21:12:41.240Z",
+    "comment": ""
+};
+
+var migrationCommands = function(transaction) {
+    return [{
+            fn: "createTable",
+            params: [
+                "users",
+                {
+                    "id": {
+                        "type": Sequelize.INTEGER,
+                        "field": "id",
+                        "autoIncrement": true,
+                        "primaryKey": true,
+                        "allowNull": false
+                    },
+                    "name": {
+                        "type": Sequelize.STRING,
+                        "field": "name"
+                    },
+                    "password": {
+                        "type": Sequelize.STRING,
+                        "field": "password"
+                    },
+                    "createdAt": {
+                        "type": Sequelize.DATE,
+                        "field": "createdAt",
+                        "allowNull": false
+                    },
+                    "updatedAt": {
+                        "type": Sequelize.DATE,
+                        "field": "updatedAt",
+                        "allowNull": false
+                    }
+                },
+                {
+                    "transaction": transaction
+                }
+            ]
+        },
+        {
+            fn: "createTable",
+            params: [
+                "melodies",
+                {
+                    "id": {
+                        "type": Sequelize.INTEGER,
+                        "field": "id",
+                        "autoIncrement": true,
+                        "primaryKey": true,
+                        "allowNull": false
+                    },
+                    "name": {
+                        "type": Sequelize.STRING,
+                        "field": "name"
+                    },
+                    "url": {
+                        "type": Sequelize.STRING,
+                        "field": "url"
+                    },
+                    "description": {
+                        "type": Sequelize.STRING,
+                        "field": "description"
+                    },
+                    "level": {
+                        "type": Sequelize.INTEGER,
+                        "field": "level"
+                    },
+                    "abcStart": {
+                        "type": Sequelize.STRING,
+                        "field": "abcStart"
+                    },
+                    "abcNotes": {
+                        "type": Sequelize.JSON,
+                        "field": "abcNotes"
+                    },
+                    "createdAt": {
+                        "type": Sequelize.DATE,
+                        "field": "createdAt",
+                        "allowNull": false
+                    },
+                    "updatedAt": {
+                        "type": Sequelize.DATE,
+                        "field": "updatedAt",
+                        "allowNull": false
+                    },
+                    "UserId": {
+                        "type": Sequelize.INTEGER,
+                        "field": "UserId",
+                        "onUpdate": "CASCADE",
+                        "onDelete": "SET NULL",
+                        "references": {
+                            "model": "users",
+                            "key": "id"
+                        },
+                        "allowNull": true
+                    }
+                },
+                {
+                    "transaction": transaction
+                }
+            ]
+        },
+        {
+            fn: "createTable",
+            params: [
+                "dictations",
+                {
+                    "id": {
+                        "type": Sequelize.INTEGER,
+                        "field": "id",
+                        "autoIncrement": true,
+                        "primaryKey": true,
+                        "allowNull": false
+                    },
+                    "score": {
+                        "type": Sequelize.INTEGER,
+                        "field": "score"
+                    },
+                    "inputObject": {
+                        "type": Sequelize.JSON,
+                        "field": "inputObject"
+                    },
+                    "createdAt": {
+                        "type": Sequelize.DATE,
+                        "field": "createdAt",
+                        "allowNull": false
+                    },
+                    "updatedAt": {
+                        "type": Sequelize.DATE,
+                        "field": "updatedAt",
+                        "allowNull": false
+                    },
+                    "UserId": {
+                        "type": Sequelize.INTEGER,
+                        "field": "UserId",
+                        "onUpdate": "CASCADE",
+                        "onDelete": "SET NULL",
+                        "references": {
+                            "model": "users",
+                            "key": "id"
+                        },
+                        "allowNull": true
+                    },
+                    "MelodyId": {
+                        "type": Sequelize.INTEGER,
+                        "field": "MelodyId",
+                        "onUpdate": "CASCADE",
+                        "onDelete": "SET NULL",
+                        "references": {
+                            "model": "melodies",
+                            "key": "id"
+                        },
+                        "allowNull": true
+                    }
+                },
+                {
+                    "transaction": transaction
+                }
+            ]
+        }
+    ];
+};
+var rollbackCommands = function(transaction) {
+    return [{
+            fn: "dropTable",
+            params: ["dictations", {
+                transaction: transaction
+            }]
+        },
+        {
+            fn: "dropTable",
+            params: ["melodies", {
+                transaction: transaction
+            }]
+        },
+        {
+            fn: "dropTable",
+            params: ["users", {
+                transaction: transaction
+            }]
+        }
+    ];
+};
+
+module.exports = {
+    pos: 0,
+    useTransaction: true,
+    execute: function(queryInterface, Sequelize, _commands)
+    {
+        var index = this.pos;
+        function run(transaction) {
+            const commands = _commands(transaction);
+            return new Promise(function(resolve, reject) {
+                function next() {
+                    if (index < commands.length)
+                    {
+                        let command = commands[index];
+                        console.log("[#"+index+"] execute: " + command.fn);
+                        index++;
+                        queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
+                    }
+                    else
+                        resolve();
+                }
+                next();
+            });
+        }
+        if (this.useTransaction) {
+            return queryInterface.sequelize.transaction(run);
+        } else {
+            return run(null);
+        }
+    },
+    up: function(queryInterface, Sequelize)
+    {
+        return this.execute(queryInterface, Sequelize, migrationCommands);
+    },
+    down: function(queryInterface, Sequelize)
+    {
+        return this.execute(queryInterface, Sequelize, rollbackCommands);
+    },
+    info: info
+};
