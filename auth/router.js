f700e18d18e4d031332dc93ff50021979200400d
Ksinia <xiatian@mail.ru>
2020-02-21 02:30:25 +0100

    Add endpoint for fetching profile by jwt

--- auth/router.js
+++ auth/router.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const { toJWT } = require("./jwt");
 const { User, Melody, Dictation, Sequelize } = require("../models");
 const bcrypt = require("bcrypt");
+const authMiddleware = require("../auth/middleware");
 
 async function login(res, next, name = null, password = null) {
   if (!name || !password) {
@@ -69,5 +70,56 @@ router.post("/login", (req, res, next) => {
   const password = req.body.password;
   login(res, next, name, password);
 });
+
+router.get(
+  "/profile", //is it ok to have this url without user id?
+  authMiddleware,
+  async (req, res, next) => {
+    const userId = req.user.id;
+    const jwt = req.headers.authorization.split(" ")[1];
+    try {
+      // I need to get user again, because I need to include all the user's information
+      const user = await User.findByPk(userId, {
+        group: ["User.id", "Melodies.id"],
+        include: {
+          model: Melody,
+          attributes: {
+            include: [
+              [
+                Sequelize.fn("COUNT", Sequelize.col("Melodies.Dictations.id")),
+                "dictationsCount"
+              ]
+            ]
+          },
+          include: [
+            {
+              model: Dictation,
+              attributes: []
+            }
+          ]
+        }
+      });
+      if (!user) {
+        res.status(400).send({
+          message: "Invalid token"
+        });
+      } else {
+        const action = {
+          type: "LOGIN_SUCCESS",
+          payload: {
+            id: user.id,
+            name: user.name,
+            jwt: jwt,
+            melodies: user.Melodies
+          }
+        };
+        const string = JSON.stringify(action);
+        res.send(string);
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 module.exports = { router, login };
