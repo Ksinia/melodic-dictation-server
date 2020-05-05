6c0cb3565a48ea862e06edd872a77b8ba3629419
Ksinia <xiatian@mail.ru>
2020-05-05 14:49:21 +0200

    Add eslint and pin prettier

--- dictation/validation.js
+++ dictation/validation.js
@@ -2,7 +2,7 @@ module.exports = (originalNotes, userInput) => {
   if (userInput.length < originalNotes.length) {
     userInput.concat(new Array(originalNotes.length - userInput.length));
   }
-  return originalNotes.map((note, index, arr) => {
+  return originalNotes.map((note, index) => {
     return note === userInput[index];
   });
 };
